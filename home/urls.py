e3348bb8a960f3c5dc26d236b6659aea130a5b14
Yakov <jakob0417@gmail.com>
2023-01-15 14:17:35 +0200

    path to new home page

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('hello/', views.say_cyprus)
+    path('hello/', views.say_cyprus),
+    path('shalom/', views.say_cheese),
 ]
--- home/views.py
+++ home/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 
 def say_cyprus(request):
     return render(request, 'hello.html', {'name': 'Yakov'})
+
+
+def say_cheese(request):
+    return render(request, 'shalom.html')
