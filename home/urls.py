ad4783e67cb746d9eb532d63c32c34e091d911a0
Yakov <jakob0417@gmail.com>
2023-01-16 16:45:12 +0200

    linking pages

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('hello/', views.say_cyprus),
-    path('shalom/', views.say_cheese),
+    path('hello/', views.say_cyprus, name='say_cyprus'),
+    path('shalom/', views.say_cheese, name='say_cheese'),
 ]
